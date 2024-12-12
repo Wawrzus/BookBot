73d16a4f6adea647f462bf0824d539607e74b72c
Szymon Wawrzyczek <szwawrzyczek@INZYNIERIA.LOCAL>
2024-12-12 10:24:06 +0100

    add dictionary sorting and printing

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ def countWords(fileContent):
 def countLetters(fileContent):
     count = {}
     for c in fileContent:
-        lower = c.lower()
-        if lower in count:
-            count[lower] += 1
-        else:
-            count[lower] = 1
+        if c.isalpha():
+            lower = c.lower()
+            if lower in count:
+                count[lower] += 1
+            else:
+                count[lower] = 1
     return count
+
+def sortList(d):
+    return d["num"]
 
 def main():
     with open("books/frankenstein.txt") as f:
         fileContent = f.read()
+    numberOfWords = countWords(fileContent)
+    numberOfLetters = countLetters(fileContent)
     print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{countWords(fileContent)} words found in the document")
-    sortedDict = sorted(countLetters(fileContent).items(), key=lambda x:x[1])
-    for c in sortedDict:
-        if c.isalpha():
-            print(f"The {c} character was found {sortedDict[c]} times")
-    print("--- End report ---")
+    print(f"{numberOfWords} words found in the document")
+    sortedList = []
+    for c in numberOfLetters:
+        sortedList.append({"char" : c, "num" : numberOfLetters[c]})
+    sortedList.sort(reverse=True, key=sortList)
+    for w in sortedList:
+        print(f"The {w['char']} character was found {w['num']} times")
 
 main()
